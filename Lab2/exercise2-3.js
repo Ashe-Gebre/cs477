25da7a3d6f6bea0d773fa679e4f850972174b4a1
Ashe-Gebre <61099470+Ashe-Gebre@users.noreply.github.com>
2021-06-25 19:28:53 -0500

    assignment 2

---
+++ Lab2/exercise2-3.js
@@ -0,0 +1,11 @@
+"use strict";
+
+  
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+    fs.createReadStream("./tiles.jpg").pipe(res);
+});
+
+server.listen(9000);
