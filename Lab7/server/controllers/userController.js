6a435ac2a1e36ca7fd0e717fada60817faa9c172
Ashenafi Gebretekle <ashenafiaynalemg@gmail.com>
2021-07-10 16:25:15 -0500

    lab 7

---
+++ Lab7/server/controllers/userController.js
@@ -0,0 +1,20 @@
+"use strict";
+
+cconst users = [];
+
+module.exports = class User {
+    constructor(username, password, role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    login() {
+        return users.find(u => { return u.username === this.username && u.password === this.password });
+    }
+
+    static init() {
+        users.push(new User('john', 'admin', 'admin'));
+        users.push(new User('bella', 'member', 'member'));
+    }
+}
